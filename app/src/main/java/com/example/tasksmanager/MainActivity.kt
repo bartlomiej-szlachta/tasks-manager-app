9fa45151531cc76db139dc3f5d5bf6d9b057ba9e
Bartłomiej Szlachta <bartlomiej.szlachta@softiq.pl>
2019-12-07 20:10:38 +0100

    create app menu

--- app/src/main/java/com/example/tasksmanager/MainActivity.kt
+++ app/src/main/java/com/example/tasksmanager/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.tasksmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.new_task -> {
+                // TODO: run new activity here
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
